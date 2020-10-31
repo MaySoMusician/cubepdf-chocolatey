627e4d95ba007ef48a00e04496c5e27d4286bdab
MaySoMusician <maysomusician@gmail.com>
2020-10-31 13:11:27 +0900

    Take over from KageShiron's repository

---
+++ cubepdf/tools/chocolateyinstall.ps1
@@ -0,0 +1,47 @@
+﻿# Taken over from https://github.com/KageShiron/chocolatey-packages/blob/9604eafb1b63c34978ac78f1ec17a68c1d4661c2/cubepdf/tools/chocolateyinstall.ps1
+# by KageShiron licensed under MIT License
+
+$ErrorActionPreference = 'Stop';
+
+$packageName= 'cubepdf'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://dl.cube-soft.jp/archive/cubepdf-1.2.0.exe'
+$url64      = 'https://dl.cube-soft.jp/archive/cubepdf-1.2.0-x64.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'cubepdf*'
+
+  # Checksums are calculated with the binaries downloaded from the official website, not the GitHub repository
+  checksum      = '3c164920912a281c283f35b26c235ed60b0f0171d30cb2685c8c4b4d900085d3'
+  checksumType  = 'sha256'
+  checksum64    = 'b8d9e0b389dd25baa67b8990c4619cdf6933f406ea52a8d099bbf224f977b022'
+  checksumType64= 'sha256'
+
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
