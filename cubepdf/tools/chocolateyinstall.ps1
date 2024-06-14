99af6e5b48b0bb019d16189d01e1196c395e498f
MaySoMusician <maysomusician@gmail.com>
2024-06-15 01:14:48 +0900

    Update cubepdf to 3.4.0

--- cubepdf/tools/chocolateyinstall.ps1
+++ cubepdf/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $ErrorActionPreference = 'Stop';
 
 $packageName= 'cubepdf'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://dl.cube-soft.jp/archive/cubepdf-1.6.1.exe'
-$url64      = 'https://dl.cube-soft.jp/archive/cubepdf-1.6.1-x64.exe'
+$url        = 'https://dl.cube-soft.jp/archive/cubepdf-3.4.0.exe'
+$url64      = 'https://dl.cube-soft.jp/archive/cubepdf-3.4.0-x64.exe'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -18,9 +18,9 @@ $packageArgs = @{
   softwareName  = 'cubepdf*'
 
   # Checksums are calculated with the binaries downloaded from the official website, not the GitHub repository
-  checksum      = '4e15f0184212c52a78c7424fae3cc695bb8e7b5a2d515f4a5f661c3389cc6f1d'
+  checksum      = ''
   checksumType  = 'sha256'
-  checksum64    = '267e008437393e1725ce219eb31a17faf7d5d2c06fad7b28c1b63967d1fbe692'
+  checksum64    = ''
   checksumType64= 'sha256'
 
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
